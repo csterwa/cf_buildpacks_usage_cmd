44fac16409c9ee3b0f410a120f371e3fefdcd63c
csterwa <chris.sterling@ctl.io>
2015-04-01 15:32:13 -0700

    Added comments for functions as good style from godoc checks

--- cf_buildpacks_usage_cmd.go
+++ cf_buildpacks_usage_cmd.go
@@ -8,29 +8,34 @@ import (
 	"github.com/cloudfoundry/cli/plugin"
 )
 
+// CliBuildpackUsage represents Buildpack Usage CLI interface
 type CliBuildpackUsage struct{}
 
+// AppSearchResults represents top level attributes of JSON response from Cloud Foundry API
 type AppSearchResults struct {
 	TotalResults int                  `json:"total_results"`
 	Resources    []AppSearchResources `json:"resources"`
 }
 
+// AppSearchResources represents resources attribute of JSON response from Cloud Foundry API
 type AppSearchResources struct {
 	Entity AppSearchEntity `json:"entity"`
 }
 
+// AppSearchEntity represents entity attribute of resources attribute within JSON response from Cloud Foundry API
 type AppSearchEntity struct {
 	Name              string `json:"name"`
 	Buildpack         string `json:"buildpack"`
 	DetectedBuildpack string `json:"detected_buildpack"`
 }
 
+// GetMetadata provides the Cloud Foundry CLI with metadata to provide user about how to use buildpack-usage command
 func (c *CliBuildpackUsage) GetMetadata() plugin.PluginMetadata {
 	return plugin.PluginMetadata{
 		Name: "CliBuildpackUsage",
 		Version: plugin.VersionType{
 			Major: 0,
-			Minor: 2,
+			Minor: 3,
 			Build: 0,
 		},
 		Commands: []plugin.Command{
@@ -49,6 +54,7 @@ func main() {
 	plugin.Start(new(CliBuildpackUsage))
 }
 
+// Run is what is executed by the Cloud Foundry CLI when the buildpack-usage command is specified
 func (c CliBuildpackUsage) Run(cliConnection plugin.CliConnection, args []string) {
 	fmt.Println("")
 	fmt.Println("Buildpacks in use across all organizations...")
@@ -61,6 +67,7 @@ func (c CliBuildpackUsage) Run(cliConnection plugin.CliConnection, args []string
 	}
 }
 
+// GetAppData requests all of the Application data from Cloud Foundry
 func (c CliBuildpackUsage) GetAppData(cliConnection plugin.CliConnection) []AppSearchResources {
 	cmd := []string{"curl", "/v2/apps"}
 	output, _ := cliConnection.CliCommandWithoutTerminalOutput(cmd...)
