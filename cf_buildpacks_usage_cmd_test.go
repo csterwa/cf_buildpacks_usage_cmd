83e72cdab70c3e0d9839e392ee800737608faf89
Michal Kuratczyk <mkuratczyk@pivotal.io>
2016-04-12 11:23:12 +0200

    add --apps flag to print apps using a given buildpack

--- cf_buildpacks_usage_cmd_test.go
+++ cf_buildpacks_usage_cmd_test.go
@@ -1,9 +1,10 @@
 package main_test
 
 import (
+	"fmt"
 	"github.com/cloudfoundry/cli/plugin/fakes"
 	io_helpers "github.com/cloudfoundry/cli/testhelpers/io"
-	. "github.com/csterwa/cf_buildpacks_usage_cmd"
+	. "github.com/mkuratczyk/cf_buildpacks_usage_cmd"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -27,7 +28,7 @@ var _ = Describe("Cloud Foundry Buildpack Usage Command", func() {
 
 			Expect(output[1]).To(Equal("2 buildpacks found across 2 app deployments"))
 			Expect(output[3]).To(Equal("Buildpacks Used"))
-			Expect(output[5]).To(Equal("Count\tName"))
+			Expect(output[5]).To(Equal(fmt.Sprintf("%-8v%-24v", "Count", "Name")))
 			Expect(output[7]).To(ContainSubstring("Java"))
 			Expect(output[8]).To(ContainSubstring("Node.js"))
 		})
@@ -51,7 +52,7 @@ var _ = Describe("Cloud Foundry Buildpack Usage Command", func() {
 				callBuildpackUsageCommandPlugin.Run(fakeCliConnection, []string{"buildpack-usage"})
 			})
 
-			Expect(output[7]).To(Equal("2\tJava"))
+			Expect(output[7]).To(Equal(fmt.Sprintf("%-8v%-24v", "2", "Java")))
 		})
 
 		It("pages through all app data to combine results", func() {
@@ -61,7 +62,19 @@ var _ = Describe("Cloud Foundry Buildpack Usage Command", func() {
 				callBuildpackUsageCommandPlugin.Run(fakeCliConnection, []string{"buildpack-usage"})
 			})
 
-			Expect(output[7]).To(Equal("4\tJava"))
+			Expect(output[7]).To(Equal(fmt.Sprintf("%-8v%-24v", "4", "Java")))
+		})
+
+		Context("when called using the --apps flag", func() {
+			It("prints which apps use a given buildpack", func() {
+				fakeAppsResponse := []string{"{\"total_pages\":1,\"total_results\":2,\"resources\":[{\"entity\":{\"name\":\"app1\",\"buildpack\":null,\"detected_buildpack\":\"Java\"}},{\"entity\":{\"name\":\"app2\",\"buildpack\":\"Java\",\"detected_buildpack\":null}}]}"}
+				fakeCliConnection.CliCommandWithoutTerminalOutputReturns(fakeAppsResponse, nil)
+				output := io_helpers.CaptureOutput(func() {
+					callBuildpackUsageCommandPlugin.Run(fakeCliConnection, []string{"buildpack-usage", "--apps"})
+				})
+
+				Expect(output[7]).To(Equal(fmt.Sprintf("%-8v%-24v%v", "2", "Java", "app1, app2")))
+			})
 		})
 	})
 })
